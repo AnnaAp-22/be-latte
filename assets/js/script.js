593f166c5d59e0b150759b7547565e59db02ce74
Anna <post-a22@mail.ru>
2018-01-09 22:15:08 +0300

    09.01

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,23 @@
-$(function(){
-});
-alert('123454 asdfghjkl zxc vb');
-// main-nav.classList.remove("main-nav--nojs");
+// $(function(){
+// });
+// alert('123454 asdfghjkl zxc vb');
+// // main-nav.classList.remove("main-nav--nojs");
+alert('3454 asdfghjkl zxc vb');
+function initialize() {
+  alert('123454 asdfghjkl zxc vb');
+  var myLatlng = new google.maps.LatLng(34.0289, -118.4777);
+  var myOptions = {
+    zoom: 8,
+    center: myLatlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(document.getElementById("map"), myOptions);
+  alert('54 asdfghjkl zxc vb');
+}
+// function initMap() {
+//   var coordinates = {lat: 47.212325, lng: 38.933663},
+//
+//     map = new google.maps.Map(document.getElementById('map'), {
+//       center: coordinates
+//     });
+// }
